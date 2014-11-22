a777231b8cd0a354913d12d5adf92bf0972463f2
陆小K <me@luxiaok.com>
2014-11-23 02:08:22 +0800

    新增公共函数接口的路由

--- xk_config/xk_url.py
+++ xk_config/xk_url.py
@@ -5,6 +5,7 @@
 from xk_handler import xk_login
 from xk_handler import xk_index
 from xk_handler import xk_test
+from xk_handler import xk_public
 
 HandlersURL = [
     (r"/(|login)/?", xk_login.LoginHandler),
@@ -12,6 +13,7 @@ HandlersURL = [
     (r"/dashboard/?", xk_index.IndexHandler),
     (r"/domain", xk_index.DomainHandler),
     (r"/record", xk_index.RecordHandler),
+    (r"/public/api", xk_public.PublicAPIHandler),
     (r"/test", xk_test.TestHandler),
     #(r"/(favicon\.ico)", tornado.web.StaticFileHandler, dict(path=settings['static_path']+"images/icon")),
 ]
