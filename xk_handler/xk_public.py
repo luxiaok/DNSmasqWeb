d4ad41569c1dafda57e37d787104f526ac9352d5
陆小K <me@luxiaok.com>
2014-11-23 03:28:22 +0800

    支持A、MX、TXT记录并优化

--- xk_handler/xk_public.py
+++ xk_handler/xk_public.py
@@ -37,11 +37,19 @@ class PublicAPIHandler(BaseHandler):
                 self.redirect("/domain")
         elif module == "dnsmasq":  # 同步解析
             if fun == "update": # 从数据库更新配置文件并重新加载服务
-                records = self.db.query("select r.record,d.domain,r.value,d.file,d.file_md5 from xk_record as r left join xk_domain as d on r.did = d.id where r.status = 'yes' and d.status = 'yes' and r.type = 'A' and r.did = %s order by d.domain,inet_aton(r.value)",id)
+                records = self.db.query("select r.record,d.domain,r.value,d.file,d.file_md5,r.type,r.priority from xk_record as r left join xk_domain as d on r.did = d.id where r.status = 'yes' and d.status = 'yes' and r.did = %s order by d.domain,inet_aton(r.value)",id)
                 # A记录
                 file_content = ''
                 for i in records:
-                    file_content += "address=/" + i['record']  + "." + i['domain'] + "/" + i['value'] + "\n"
+                    if i['type'] == "A":
+                        file_content += "address=/" + i['record']  + "." + i['domain'] + "/" + i['value'] + "\n"
+                    elif i['type'] == "MX":
+                        file_content += "mx-host=" + i["domain"] + "," + i["value"] + "," + str(i['priority']) + "\n"
+                    elif i['type'] == "TXT":
+                        file_content += "txt-record="
+                        if i["record"] != "@":
+                            file_content += i['record'] + "."
+                        file_content += i["domain"] + ',"' + i["value"] + '"\n'
                 f = open("/etc/dnsmasq.d/" + i['file'],"w")
                 f.write(file_content)
                 f.close()
