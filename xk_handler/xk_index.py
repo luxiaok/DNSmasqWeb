a380b59a1d08de34c6778687dd13418bd2cb470f
陆小K <me@luxiaok.com>
2014-11-23 21:44:35 +0800

    优化首页动态数据

--- xk_handler/xk_index.py
+++ xk_handler/xk_index.py
@@ -4,9 +4,19 @@
 from xk_application.xk_main import *
 
 class IndexHandler(BaseHandler):
+    # 获取服务器运行时间
+    def get_uptime(self):
+        f = open('/proc/uptime','r')
+        r = f.read()
+        u = r.split()
+        f.close()
+        uptime = self.format_seconds(int(float(u[0])))
+        return uptime
+
     @Auth
     def get(self):
         #print self.get_login_url()
         #print self.current_user
         #print self.user_info()
-        self.render2("xk_index.html",dashboard="active")
+        data = {"uptime":self.get_uptime()}
+        self.render2("xk_index.html",dashboard="active",data=data)
