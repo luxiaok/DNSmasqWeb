557d35cfc7cb255d02c4dad8d9df558123a301f3
陆小K <me@luxiaok.com>
2014-11-26 00:55:48 +0800

    优化DHCP后台数据结构

--- xk_handler/xk_dhcp.py
+++ xk_handler/xk_dhcp.py
@@ -7,23 +7,42 @@ class DhcpPoolHandler(BaseHandler):
     @Auth
     def get(self):
         #
-        dhcps = self.db.query("select * from xk_dhcp_pool")
-        self.render2("xk_dhcp_pool.html",dhcps=dhcps,dhcp_pool="active")
+        dhcp_options = self.db.query('''select * from xk_options where type = "dhcp"''')
+        dhcp = {}
+        for i in dhcp_options:
+            dhcp[i['name']] = i['value']
+        self.render2("xk_dhcp_pool.html",dhcp=dhcp,dhcp_pool="active")
 
     @Auth
     def post(self):
-        name = self.get_argument("name")
+        status = self.get_argument("status")
         range_start = self.get_argument("range_start")
         range_end = self.get_argument("range_end")
         netmask = self.get_argument("netmask")
+        lease = self.get_argument("lease")
         router = self.get_argument("router")
         dns1 = self.get_argument("dns1")
         dns2 = self.get_argument("dns2")
         domain = self.get_argument("domain")
-        lease = self.get_argument("lease")
+        ntp = self.get_argument("ntp",'')
         comment = self.get_argument("comment")
-        self.db.execute('''insert into xk_dhcp_pool ( name,range_start,range_end,netmask,router,dns1,dns2,domain,lease,comment )
-            values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',name,range_start,range_end,netmask,router,dns1,dns2,domain,lease,comment)
+        #self.db.execute('''insert into xk_dhcp_pool ( name,range_start,range_end,netmask,router,dns1,dns2,domain,lease,comment )
+        #    values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',name,range_start,range_end,netmask,router,dns1,dns2,domain,lease,comment)
+        self.db.execute('''
+            insert into xk_options (name,value,comment) values
+            ('xk_dhcp_status',%s,'DHCP开关'),
+            ('xk_dhcp_pool_start',%s,'DHCP地址池开始地址'),
+            ('xk_dhcp_pool_stop',%s,'DHCP地址池结束地址'),
+            ('xk_dhcp_pool_netmask',%s,'DHCP地址池子网掩码'),
+            ('xk_dhcp_pool_lease',%s,'DHCP租约'),
+            ('xk_dhcp_pool_gw',%s,'DHCP默认网关'),
+            ('xk_dhcp_pool_dns1',%s,'DHCP主DNS服务器'),
+            ('xk_dhcp_pool_dns2',%s,'DHCP辅助DNS服务器'),
+            ('xk_dhcp_pool_domain',%s,'DHCP缺省域名'),
+            ('xk_dhcp_pool_ntp',%s,'DHCP时间服务器'),
+            ('xk_dhcp_pool_comment',%s,'DHCP地址池备注')
+            ON DUPLICATE KEY UPDATE name=values(name),value=values(value),comment=values(comment)
+         ''',status,range_start,range_end,netmask,lease,router,dns1,dns2,domain,ntp,comment)
         self.write("1")
 
 class DhcpHostHandler(BaseHandler):
