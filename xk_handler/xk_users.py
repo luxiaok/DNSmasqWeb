bf467e8debc20c070df62dfc1d6b2a31406b6419
陆小K <me@luxiaok.com>
2014-12-05 23:33:35 +0800

    新增添加用户功能

--- xk_handler/xk_users.py
+++ xk_handler/xk_users.py
@@ -7,3 +7,21 @@ class UsersHandler(BaseHandler):
     def get(self):
         users = self.db.query("select * from xk_users")
         self.render2("xk_users.html",users=users,users_admin="active")
+
+    def post(self):
+        username = self.get_argument("username")
+        name = self.get_argument("name")
+        email = self.get_argument("email")
+        mobile = self.get_argument("mobile")
+        password = self.get_argument("password")
+        comment = self.get_argument("comment")
+        fun = self.get_argument("fun")
+        if fun == "add":
+            check_user = self.db.get("select id,username,name from xk_users where username = %s",username)
+            if check_user:
+                self.write("2")
+                return
+            self.db.execute("insert into xk_users (username,name,email,mobile,password,comment,cdate) values (%s,%s,%s,%s,md5(%s),%s,CURRENT_TIMESTAMP)",username,name,email,mobile,password,comment)
+            self.write("1")
+            return
+
